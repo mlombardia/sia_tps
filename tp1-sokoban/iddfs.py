dd15f7ce89c2ed67af8b0f60e416df2c9553e6df
Cami <cponce@itba.edu.ar>
2021-08-13 10:58:13 -0300

    mini cambio iddfs

--- tp1-sokoban/iddfs.py
+++ tp1-sokoban/iddfs.py
@@ -7,22 +7,20 @@ BLOCK = 5
 class IDDFS(SearchMethod):
 
     maxDepth = 0
-    node_stack=[]
 
     def __init__(self, maxDepth):
         self.maxDepth = maxDepth       #profundidad maxima
-        self.node_stack=[]
         super().__init__()
 
     def search(self, game_map):
         startDepth=0
         visited_nodes = set()
 
-        self.node_stack = [Node((game_map.playerFila, game_map.playerColumna), None, None, game_map.boxes)]  # arranco algoritmo con la posicion inicial del player, no tiene previos ni ninguna direccion asociada
+        node_stack = [Node((game_map.playerFila, game_map.playerColumna), None, None, game_map.boxes)]  # arranco algoritmo con la posicion inicial del player, no tiene previos ni ninguna direccion asociada
 
-        while len(self.node_stack) > 0:
+        while len(node_stack) > 0:
             while startDepth < self.maxDepth:
-                current = self.node_stack.pop()                                             # agarro un nodo, lo agrego a visitados
+                current = node_stack.pop()                                             # agarro un nodo, lo agrego a visitados
                 visited_nodes.add(current)
                 if game_map.check_if_win(current):
                     print("Solution found! Calculating...")
@@ -34,7 +32,7 @@ class IDDFS(SearchMethod):
                     for move in new_moves:                                                  # sino, chequeo que movs disponibles
                         if move not in visited_nodes:                                       # y los agrego al stack si no fue visitado
                             startDepth += 1                                                 # le sumo 1 a la profundidad
-                            self.node_stack.append(move)
+                            node_stack.append(move)
             startDepth = self.maxDepth                                                      #busco "un nivel" mas abajo
             self.maxDepth += BLOCK
         return None
