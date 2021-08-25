8d2e29da351d08273e143d5ac4a0f4589cd02258
Cami <cponce@itba.edu.ar>
2021-08-25 20:14:32 -0300

    armas test

---
+++ tp2_genetics/main.py
@@ -0,0 +1,8 @@
+import linecache
+
+
+weapon = linecache.getline('armasTest.txt', 1)
+boots = open('botasTest.txt')
+helmet = open('cascosTest.txt')
+gloves = open('guantesTest.txt')
+vest = open('pecherasTest.txt')
