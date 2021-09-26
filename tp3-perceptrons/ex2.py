3faf25ea73c594ae78b5fd19e3d1f4f07df1901a
Cami <cponce@itba.edu.ar>
2021-09-26 11:48:05 -0300

    Auto stash before rebase of "origin/main"

--- tp3-perceptrons/ex2.py
+++ tp3-perceptrons/ex2.py
@@ -42,14 +42,15 @@ def ex2(config):
         perceptron_lineal = SimplePerceptron(training_set.shape[1], lineal_act, der_lineal_act,
                                              eta=0.01)  # aprende poquito
         min_err, epochs, error_list, training_accuracies, min_err_test, test_accuracies = perceptron_lineal.train(training_set, training_expected, test_set, test_expected)
-        min_err_test, test_accuracies = perceptron_lineal.test(test_set, test_expected)
+        # min_err_test, test_accuracies = perceptron_lineal.test(test_set, test_expected)
         print()
         print("training error", min_err)
         print("testing error", min_err_test)
         print()
 
-        plot(epochs, [training_accuracies, test_accuracies], ['train acc', 'test acc'], 'Epoch', 'Accuracies', 'Accuracies vs Epochs - linear')
         plot(epochs, [error_list], ['error'], 'Epoch', 'Errors', 'Mean Squared Error vs Epochs - linear')
+        plot(epochs, [training_accuracies, test_accuracies], ['train acc', 'test acc'], 'Epoch', 'Accuracies',
+             'Accuracies vs Epochs - linear')
 
     elif config['perceptron_type'] == 'non_linear_tanh':
         perceptron_nolineal2 = SimplePerceptron(training_set.shape[1], tanh_act, der_tanh_act, eta=0.01)
@@ -65,7 +66,7 @@ def ex2(config):
         plot(epochs, [error_list], ['error'], 'Epoch', 'Errors', 'Mean Squared Error vs Epochs - tanh')
 
     elif config['perceptron_type'] == 'non_linear_sigmoid':
-        perceptron_nolineal2 = SimplePerceptron(training_set.shape[1], sigmoid_act, der_sigmoid_act, eta=0.01)
+        perceptron_nolineal2 = SimplePerceptron(training_set.shape[1], sigmoid_act, der_sigmoid_act, eta=0.1)
         min_err, epochs, error_list, training_accuracies, min_err_test, test_accuracies = perceptron_nolineal2.train(training_set, training_expected, test_set, test_expected)
         # min_err_test, test_accuracies = perceptron_nolineal2.test(test_set, test_expected)
 
@@ -74,5 +75,5 @@ def ex2(config):
         print("testing error", min_err_test)
         print()
 
-        plot(epochs, [training_accuracies, test_accuracies], ['train acc', 'test acc'], 'Epoch', 'Accuracies', 'Accuracies vs Epochs - sigmoid')
-        plot(epochs, [error_list], ['error'], 'Epoch', 'Errors', 'Mean Squared Error vs Epochs - sigmoid')
+        #plot(epochs, [training_accuracies, test_accuracies], ['train acc', 'test acc'], 'Epoch', 'Accuracies', 'Accuracies vs Epochs - sigmoid')
+        #plot(epochs, [error_list], ['error'], 'Epoch', 'Errors', 'Mean Squared Error vs Epochs - sigmoid')
